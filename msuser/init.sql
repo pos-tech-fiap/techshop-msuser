66df39568cf75c3f8c86e5d4066d109d67ab6209
eugabiviana <gabi.viana88@gmail.com>
2024-03-22 23:33:33 -0300

    Modificando init.sql

--- msuser/init.sql
+++ msuser/init.sql
@@ -5,9 +5,12 @@
 \c techshop;
 
 CREATE TABLE tb_user (
-                            id BIGINT PRIMARY KEY,
-                            name VARCHAR(255) not null,
-                            birthDate VARCHAR(255) not null,
-                            cpf BIGINT not null,
-                            value double precision not null
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255) not null,
+    birthDate VARCHAR(255) not null,
+    cpf BIGINT not null,
+    address VARCHAR(255) not null,
+    mobile VARCHAR(255) not null,
+    email VARCHAR(255) not null,
+    value double precision not null
 );
