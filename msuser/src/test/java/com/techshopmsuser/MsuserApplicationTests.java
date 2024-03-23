591d31a15ace4e6567bb3505e803c68d437d5a19
eugabiviana <gabi.viana88@gmail.com>
2024-03-22 21:16:14 -0300

    Criando projeto msuser e docker-compose

---
+++ msuser/src/test/java/com/techshopmsuser/MsuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techshopmsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
