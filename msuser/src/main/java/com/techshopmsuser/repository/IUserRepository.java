30e6f0d0653f01f6ca6e6bed011d22c5e19a498f
eugabiviana <gabi.viana88@gmail.com>
2024-03-22 22:41:24 -0300

    Refatorando Repository

--- msuser/src/main/java/com/techshopmsuser/repository/IUserRepository.java
+++ msuser/src/main/java/com/techshopmsuser/repository/IUserRepository.java
@@ -1,8 +1,11 @@
 package com.techshopmsuser.repository;
 
+import com.techshopmsuser.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface IUserRepository extends JpaRepository<User, UUID> {
 }
